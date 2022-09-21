14af4813f385395cbb4a4404b17801b0749fd2ad
emiruyan <emir-uyn@hotmail.com>
2022-09-21 20:16:15 +0300

    Player Movement Controller
    We added a movement to our character in the x and z plane.

---
+++ AtmRushReplica/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float horizontalSpeed;
+    [SerializeField] private float horizontalLimit;
+    private float _horizontal;
+
+
+    private void Update()
+    {
+        HorizontalMovementInput();
+        ForwardMovement();
+    }
+
+    private void HorizontalMovementInput()
+    {
+        float _newX;
+        
+        if (Input.GetMouseButton(0))
+        {
+            _horizontal = Input.GetAxisRaw("Mouse X");
+        }
+        
+        _newX = transform.position.x + _horizontal * horizontalSpeed * Time.deltaTime;
+        _newX = Math.Clamp(_newX, -horizontalLimit, horizontalLimit);
+
+        transform.position = new Vector3(
+            _newX,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+
+    private void ForwardMovement()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
